779500f1fc7fc888b3d567d6fa7cfd0031d1330b
Ernest Lim <ernestlimwoonteng@gmail.com>
2023-07-07 14:58:35 +0800

    [TILING] converted to local display

--- opencv_tiling/tiling.py
+++ opencv_tiling/tiling.py
@@ -26,34 +26,39 @@ REDIS_HOST="localhost"
 REDIS_PORT="6379"
 REDIS_PASSWORD=""
 
-producer = Producer({
-    "bootstrap.servers": KAFKA_SERVER
-})
+VIEW_ON_KAFKA=False
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
+def setup_kafka_redis():
+    producer = Producer({
+        "bootstrap.servers": KAFKA_SERVER
+    })
+
+    redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 
-admin_client = AdminClient({
-    "bootstrap.servers": KAFKA_SERVER
-})
+    admin_client = AdminClient({
+        "bootstrap.servers": KAFKA_SERVER
+    })
 
-topic_list = [KAFKA_MONITOR_TOPIC]
+    topic_list = [KAFKA_MONITOR_TOPIC]
 
-kafka_topic_list = []
-for topic in topic_list:
-    new_topic = NewTopic(topic, 1, 1)
-    kafka_topic_list.append(new_topic)
-fs = admin_client.create_topics(kafka_topic_list)
+    kafka_topic_list = []
+    for topic in topic_list:
+        new_topic = NewTopic(topic, 1, 1)
+        kafka_topic_list.append(new_topic)
+    fs = admin_client.create_topics(kafka_topic_list)
 
-for topic, f in fs.items():
-    try:
-        f.result()  # The result itself is None
-        print("Topic {} created".format(topic))
-    except Exception as e:
-        if e.args[0].code() == KafkaError.TOPIC_ALREADY_EXISTS:
-            print("Topic {} already created".format(topic))
-        else:
-            print("Failed to create topic {}: {}".format(topic, e))
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} created".format(topic))
+        except Exception as e:
+            if e.args[0].code() == KafkaError.TOPIC_ALREADY_EXISTS:
+                print("Topic {} already created".format(topic))
+            else:
+                print("Failed to create topic {}: {}".format(topic, e))
+
+    return producer, redis_client
 
 def draw_text(
         img, text,
@@ -110,14 +115,13 @@ def stack_frames(frames, num_horizontal, frame_size=(120,90)):
     
     return big_frame
 
-def store_frame_in_redis(frame):
+def store_frame_in_redis(frame, redis_client):
     key = datetime.now().strftime(DATETIME_FORMAT)
 
     redis_client.set(key, frame)
     # set the expiration time for the key to 1 minute
     redis_client.expire(key, 10)
     return key
-
 
 video_cap_list = []
 
@@ -131,11 +135,18 @@ while True:
             FRAMES[i] = f
     big_frame = stack_frames(FRAMES, 3, frame_size=(480, 360))
 
-    _, buffer = cv2.imencode('.jpg', big_frame)
+    cv2.imshow("monitor", big_frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
 
-    key = store_frame_in_redis(buffer.tobytes())
-    msg = {
-        "key": key
-    }
-    msg_str = json.dumps(msg)
-    producer.produce(KAFKA_MONITOR_TOPIC, msg_str)
+cv2.destroyAllWindows()
+
+    # producer, redis_client = setup_kafka_redis()
+    # _, buffer = cv2.imencode('.jpg', big_frame)
+
+    # key = store_frame_in_redis(buffer.tobytes(), redis_client)
+    # msg = {
+    #     "key": key
+    # }
+    # msg_str = json.dumps(msg)
+    # producer.produce(KAFKA_MONITOR_TOPIC, msg_str)
